745f4124f0cf31f9ab7119b33a8ec653b29c8f72
CoollbreackerzSdo <ralberinfante4@gmail.com>
2024-12-17 14:04:55 -0400

    create common configuration

---
+++ UrlsMania.Server/Common/Context/BaseEntityConfiguration.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using UrlsMania.Server.Common.Models;
+
+namespace UrlsMania.Server.Common.Context;
+
+public abstract class BaseEntityConfiguration<T> : IEntityTypeConfiguration<T>
+    where T : EntityBase
+{
+    public virtual void Configure(EntityTypeBuilder<T> builder)
+    {
+        builder.Property(x => x.Key)
+            .HasConversion(x => x, x => x)
+            .HasColumnName("id");
+            
+        builder.HasKey(x => x.Key);
+    }
+}
