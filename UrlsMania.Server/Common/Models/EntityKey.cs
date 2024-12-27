fac57f436ba12b241f98a5ed45036c0e1e00ef84
CoollbreackerzSdo <ralberinfante4@gmail.com>
2024-12-27 09:28:14 -0400

    change to value record

--- UrlsMania.Server/Common/Models/EntityKey.cs
+++ UrlsMania.Server/Common/Models/EntityKey.cs
@@ -1,9 +1,10 @@
 namespace UrlsMania.Server.Common.Models;
 
-public record EntityKey<T>(T Value) : IComparable<EntityKey<T>>
+public record struct EntityKey<T>(T Value) : IComparable<EntityKey<T>>
     where T : notnull, IComparable<T>
 {
-    public int CompareTo(EntityKey<T>? other) => Value.CompareTo(other!.Value);
+    public T Value { get; init; } = Value;
+    public readonly int CompareTo(EntityKey<T> other) => Value.CompareTo(other!.Value);
     public static implicit operator T(EntityKey<T> key) => key.Value;
     public static implicit operator EntityKey<T>(T value) => new(value);
 }
