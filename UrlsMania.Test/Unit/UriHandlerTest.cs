beeb8eef8d84ee1647a8bbc0869a017747615a00
CoollbreackerzSdo <ralberinfante4@gmail.com>
2024-12-27 09:36:03 -0400

    create test

---
+++ UrlsMania.Test/Unit/UriHandlerTest.cs
@@ -0,0 +1,14 @@
+namespace UrlsMania.Test.Unit;
+
+public class UriHandlerTest
+{
+    [Fact]
+    public void CreateNewUriValid_ReturnCreate()
+    {
+        // Given
+    
+        // When
+    
+        // Then
+    }
+}
