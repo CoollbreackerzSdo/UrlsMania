e1e31f852735be9e8bdd41c50f46588c3d2fa4ab
CoollbreackerzSdo <ralberinfante4@gmail.com>
2024-12-27 09:31:37 -0400

    register services

--- UrlsMania.Server/Program.cs
+++ UrlsMania.Server/Program.cs
@@ -1,10 +1,16 @@
 using Scalar.AspNetCore;
 
+using UrlsMania.Server.Url.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Host.ConfigureSerilog();
 builder.Services.AddOpenApi();
+builder.Services.AddHandler();
+builder.Services.AddMiddlewares();
+builder.Services.AddGenerators();
 builder.AddContexts();
 builder.AddServiceDefaults();
 
@@ -13,6 +19,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MapMigrations();
     app.MapOpenApi();
     app.MapScalarApiReference(options =>
     {
@@ -25,5 +32,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapMiddlewares();
 app.MapDefaultEndpoints();
+app.MapUrlEndpoints();
 app.Run();
--- UrlsMania.Server/ServiceDiscovery.cs
+++ UrlsMania.Server/ServiceDiscovery.cs
@@ -2,8 +2,11 @@ using EntityFramework.Exceptions.PostgreSQL;
 
 using Serilog;
 
+using UrlsMania.Server.Common.Handler;
 using UrlsMania.Server.Common.Middlewares;
 using UrlsMania.Server.Url.Context;
+using UrlsMania.Server.Url.Handler;
+using UrlsMania.Server.Url.Models;
 using UrlsMania.Server.Url.Services.Generators;
 
 namespace UrlsMania.Server;
@@ -17,11 +20,19 @@ public static class ServiceDiscovery
     public static IApplicationBuilder MapMiddlewares(this IApplicationBuilder builder)
         => builder.UseMiddleware<ErrorMiddleware>();
     public static IServiceCollection AddGenerators(this IServiceCollection services)
-        => services.AddTransient<ITextRandomGenerador, RandomTextGenerator>();
+        => services.AddTransient<ITextRandomGenerador, RandomUriCode>();
     public static IHostApplicationBuilder AddContexts(this IHostApplicationBuilder builder)
     {
-        builder.AddNpgsqlDbContext<UriContext>("postgres", null, options => options.UseExceptionProcessor());
-        // builder.Services.AddDbContext<UriContext>(options => options.UseNpgsql().UseExceptionProcessor());
+        // builder.AddNpgsqlDbContext<UriContext>("postgres", null, options => options.UseExceptionProcessor());
+        builder.Services.AddDbContext<UriContext>(options => options.UseNpgsql().UseExceptionProcessor());
+        builder.Services.AddTransient<IUriRepository, UriRepository>();
         return builder;
     }
+    public static void MapMigrations(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        scope.ServiceProvider.GetRequiredService<UriContext>().Database.Migrate();
+    }
+    public static IServiceCollection AddHandler(this IServiceCollection services)
+        => services.AddTransient<IHandler<(ShortUrlRequest Request, string UrlBase), string>, CreateNewUriHandler>();
 }
